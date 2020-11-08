12cb6cc5f3e5497d397ffc82efc3a1296fdc2673
danilovarushchik <dan.varushchik@gmail.com>
2020-11-08 19:21:22 +0200

    adding login and registration functionality

--- database.py
+++ database.py
@@ -1,23 +1,42 @@
 import sqlite3
 DATABASE_NAME = "project_database.db"
 
+
 class Database:
     connection = None
     cursor = None
 
     def __init__(self):
-        self.connection = sqlite3.connect(DATABASE_NAME)
+        self.connection = sqlite3.connect(DATABASE_NAME, check_same_thread=False)
         self.cursor = self.connection.cursor()
 
-    def perform_query(self, query):
-        self.cursor.execute(query)
-        self.connection.commit()
+    def _perform_query(self, query):
+        try:
+            self.cursor.execute(query)
+            self.connection.commit()
+            return True, None
+        except sqlite3.OperationalError as error:
+            return False, str(error)
 
-    def add_user(self):
-        pass
+    def add_user(self, email, password):
+        query = "INSERT INTO user (email,password) VALUES( '{}',	'{}')".format(email, password)
+        try:
+            return self._perform_query(query)
+        except sqlite3.IntegrityError as error:
+            if "UNIQUE constraint failed: user.email" == str(error):
+                return False, "This email is already registered"
+            else:
+                return False, error
 
-    def check_user(self):
-        pass
+    def check_user(self, email, password):
+        query = "SELECT * FROM user WHERE email='{}' AND password='{}'".format(email, password)
+        print(query)
+        result = self._perform_query(query)
+        if not result[0]:
+            return result
+        else:
+            rows = self.cursor.fetchall()
+            return True, len(rows) == 1
 
     def add_rating(self):
         pass
--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 from database import Database
+from flask import Flask, request
+SERVER_NAME = "Server"
+IP_ADDRESS = "0.0.0.0"
+PORT = 105
 
 
-def main(test_mode=False):
-    local_storage = {"sessions": []}
-    db = Database()
-    if test_mode:
-        test()
-    else:
-        pass
+app = Flask(SERVER_NAME)
+app.config["DEBUG"] = True
+db = Database()
 
 
-def test():
-    pass
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    response = db.check_user(request.args["email"], request.args["password"])
+    if response[0]:
+        if response[1]:
+            return "User in DB"
+        else:
+            return "User not in DB"
 
 
-if __name__ == "__main__":
-    main()
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    response = db.add_user(request.args["email"], request.args["password"])
+    return str(response)
+
+
+app.run(host=IP_ADDRESS, port=PORT)
