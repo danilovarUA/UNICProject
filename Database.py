81ff5daf9e538fd8560978b31e0121ba4fa04874
danilovarushchik <dan.varushchik@gmail.com>
2021-01-25 12:29:32 +0200

    corrections to Database so it can work even if imported

--- Database.py
+++ Database.py
@@ -1,6 +1,6 @@
 import sqlite3
 import re
-DATABASE_NAME = "project_database.db"
+DATABASE_NAME = "../data/project_database.db"
 
 
 def regexp(expr, item):
@@ -12,7 +12,7 @@ class Database:
     connection = None
     cursor = None
 
-    def __init__(self, debug=True):
+    def __init__(self, debug=False):
         self.debug = debug
         self.connection = sqlite3.connect(DATABASE_NAME, check_same_thread=False)
         self.connection.create_function("REGEXP", 2, regexp)
@@ -44,7 +44,7 @@ class Database:
         if result[0]:
             data = self.cursor.fetchall()
             if self.debug:
-                print("Date returned: {}".format(data))
+                print("Data returned: {}".format(data))
             return True, data
         return result
 
@@ -61,27 +61,23 @@ class Database:
             print("Database created insert query: {}".format(query))
         return self._perform_query_(query)
 
+    def delete(self, data, table):
+        selectors = []
+        for name in data:
+            value = data[name]
+            selectors.append("{} REGEXP '{}'".format(name, value))
+        query = "DELETE FROM {} WHERE {}".format(table, " AND ".join(selectors))
+        if self.debug:
+            print("Database created delete query: {}".format(query))
+        result = self._perform_query_(query)
+        if result[0]:
+            data = self.cursor.fetchall()
+            if self.debug:
+                print("Data returned: {}".format(data))
+            return True, data
+        return result
+
     def __del__(self):
         self.connection.close()
         if self.debug:
             print("Database was destroyed")
-
-
-if __name__ == "__main__":
-    # populate movies
-    # movies = ["Titanic", "The Wizard of Oz", "Star Wars: Episode IV - A New Hope",
-    #           "The Lord of the Rings: The Return of the King", "Snow White and the Seven Dwarfs ",
-    #           "Terminator 2: Judgment Day", "The Lion King", "The Godfather", "The Jesus Film", "Jurassic Park",
-    #           "Raiders of the Lost Ark", "The Shawshank Redemption", "The Dark Knight",
-    #           "Pirates of the Caribbean: The Curse of the Black Pearl", "Jaws", "Fight Club", "Pulp Fiction",
-    #           "Forrest Gump", "Shrek", "Transformers", "The Matrix"]
-    # db = Database()
-    # for mov in movies:
-    #     query = "INSERT INTO movie (name) VALUES( '{}')".format(mov)
-    #     print (db._perform_query(query))
-
-    # movie search test
-    db = Database()
-    res = db.select({"name": '^$'}, 'movie')
-    print(res)
-
